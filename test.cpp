fe7562e878c9e16c38a7564f9e846ff06626b577
Rishabhhacker <rishabhrai798@gmail.com>
2019-04-10 09:28:50 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,241 +1,133 @@
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <pthread.h>
-
-#include <unistd.h>
-
-
-
-
-
-
-
-#define MIN_PID 100
-
-#define MAX_PID 1000
-
-
-
-int size_of_thread;
-
-int thread_number = 0;
-
-pthread_mutex_t mutex;
-
-
-
-
-
-struct pid_tab
-
-{
-
-    int pid;
-
-    float bitmap;
-
-}pidArr[1000];
-
-
-
-int allocate_map(void)                                  //allocates bitmap values to the data structure
-
-{
-
-    int i,j;
-
-    for(i = MIN_PID, j =0; i <= MAX_PID; i++, j++)
-
-    {
-
-        pidArr[j].pid = i;
-
-        pidArr[j].bitmap = 0;
-
-    }
-
-
-
-    if(i == MAX_PID && j == MAX_PID)
-
-    {
-
-    return 1;
-
-	}
-
-    else
-
-    {
-
-	return -1;
-
-	}
-
-}
-
-
-
-int allocate_pid(void)                                  //allocates a pid to the new process
-
-{
-
-	int i,j;
-
-    for(i = MIN_PID, j =0; i <= MAX_PID; i++, j++)
-
-    {
-
-        if(pidArr[j].bitmap == 0)
-
-        {
-
-            pidArr[j].pid = i;
-
-            pidArr[j].bitmap = 1;
-
-            return i;
-
-            break;
-
-        }
-
-    }
-
-
-
-}
-
-
-
-void release_pid(int rpid)                               //releases pid
-
-{
-
-	int i;
-
-    for(i = 0; i <= MAX_PID; i++)
-
-    {
-
-        if(pidArr[i].pid == rpid)
-
-        {
-
-            pidArr[i].bitmap = 0;
-
-        }
-
-    }
-
-}
-
-
-
-
-
-void * Multi_Thread(void * v)                          //function called by the created thread
-
-{
-
-    int rpid = allocate_pid();       //allocates a pid
-
-	while (thread_number < size_of_thread)
-
-    {
-
-        pthread_mutex_lock(&mutex);     //mutex lock occurs
-
-        if (thread_number >= size_of_thread)
-
-        {
-
-            pthread_mutex_unlock(&mutex);
-
-            break;
-
-        }
-
-        thread_number++;                    //threadVar increments at least once
-
-        //sleep(100);
-
-        printf("\n\t###############################################");
-
-        printf("\n\t##   Thread %d    ##   Succesfully Created   ##",thread_number);
-
-        pthread_mutex_unlock(&mutex);      //mutex now unlocked
-
-    }
-
-    //sleep(5);
-
-    release_pid(rpid);           //pid released
-
-
-
-}
-
-
+#include<stdio.h>
 
 int main()
-
 {
+	int n,i,j,k;
+	/*
+	at---arrival time
+	bt--burst time
+	ft---finish time
+	tat---turn around time
+	rm---remaining time
+	finished----to check if process is finished
+	*/
+	int at[20],bt[20],ft[20],pn[20],tat[20],wt[20],finished[20],rm[20],priority[20];
+	printf("Enter the number of processes: ");
+	scanf("%d",&n);
 
-    int i =0;
+	//taking arrival time and burst time for n processes and initialing isFinished to false and remaining time to burst time
+	printf("Enter at, bt and priority of processes: \n");
+	for(i=0;i<n;i++)
+	{
+		pn[i]=i+1;
+		scanf("%d %d %d",&at[i],&bt[i],&p[i]);
+		finished[i]=0;
+		rm[i]=bt[i];
 
-    printf("\n\tEnter How Many Number of Threads You Want : ");
+	}
+	
+	
+	//sorting the processes on arrival time(ascending order)
+	
+	for( i=0;i<n;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(at[j]>at[j+1])
+			{
+				swap(&at[j],&at[j+1]);
+				swap(&bt[j],&bt[j+1]);
+				swap(&pn[j],&pn[j+1]);
+			}
+		}
 
-    scanf("%d",&size_of_thread);
+	}
 
-    pthread_t thread[size_of_thread];
+	
+	
+	int total_time,all_fin=0;
+	int min;
+	int pos=0,flag=0;
 
-
-
-	printf("\n\t###############################################");
-
-	printf("\n\t##                                           ##");
-
-	printf("\n\t##          Creating %d Threads              ##",size_of_thread);
-
-	printf("\n\t##                                           ##");
-
-	printf("\n\t###############################################");
-
-
-
-//   sleep(1000);        //delay only so that the above can be read in output screen before execution of the rest of the code
-
-
-
-    for(i = 0; i < size_of_thread; i++)
-
-    {
-
-        pthread_mutex_init(&mutex, NULL);
-
-        pthread_create(&thread[i], NULL, Multi_Thread, NULL);
-
-        Multi_Thread(NULL);
-
-    }
+	 total_time=at[0];
 
 
+	//while all processes are not finished, find the process with lowest ie highest priority and process it.At each interval keep checking the upcoming processes for higher priority
+	//total_time----total time elapsed
+	while(!all_fin)
+	{
+		min=99999;
+		flag=0;
+		for(j=0;j<n;j++)
+		{
+		   //checking for process that has arrived but not yet finished (having highest priority at that time)
+		    if(at[j]<=total_time && finished[j]==0 && p[j]<min)
+		    {
+		        min=p[i];
+		        pos=j;
+		        flag=1;
+		    }
 
-    for(i = 0; i < size_of_thread; i++)
+		}
+		if(!flag)
+		{
+		    for(i=0;i<n;i++)
+		    {
+                 if(finished[i]==0)
+                {
+                    total_time=at[i];
+                    break;
+                }
+		    }
 
-    {
+		}
+		else
+		{
+			total_time++;
+			rm[pos]--;
 
-        pthread_join(thread[i], NULL);
+			if(rm[pos]==0)
+			{
 
-        pthread_mutex_destroy(&mutex);
+				finished[pos]=1;
+				ft[pos]=total_time;
+			}
+		}
+		all_fin=1;
+			for(i=0;i<n;i++)
+		{
+			if(finished[i]==0)
+			{
+				all_fin=0;
+				break;
+			}
+		}
 
-    }
 
-	printf("\n\t###############################################");
+	}
+	printf("\n");
+	
+	//total_wt---total waiting time
+	//total_tat--total turn around time
 
-    return 0;
+	float total_wt=0,total_tat=0;
+	
+	//calculating total turn around time, waiting time, totat waiting time and total turn around time using basic formule
+	for(i=0;i<n;i++)
+	{
+		tat[i]=ft[i]-at[i];
+		wt[i]=tat[i]-bt[i];
+		total_wt+=wt[i];
+		total_tat+=tat[i];
+	}
+	
 
+	//printing arrival time, burst time, finish time, turn around time and waiting time for all processes in tabular form
+	printf("Process \t AT \t BT \t p \t FT \t TAT \t WT\n");
+	for(i=0;i<n;i++)
+		printf("P%d \t\t %d \t %d \t %d \t\t %d \t %d \t %d\n",pn[i],at[i],bt[i],p[i],ft[i],tat[i],wt[i]);
+
+	//calculating average turn around time and average waiting time
+	printf("\nAverage TAT: %f\n",total_tat/n);
+	printf("Average WT: %f\n",total_wt/n);
 }
