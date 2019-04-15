b8496bae5cac2f4347a6d8b65918231bb445535a
Rishabhhacker <rishabhrai798@gmail.com>
2019-04-15 23:40:44 +0530

    Update test2.c

--- test2.c
+++ test2.c
@@ -1,4 +1,12 @@
-//gcc -Wall -pthread 
+/*The Sleeping-Barber Problem. A barbershop consists of a waiting room with n chairs
+and a barber room with one barber chair. If there are no customers to be served the
+barber goes to sleep. If a customer enters the barbershop and all chairs are occupied
+then the customer leaves the shop. If the barber is busy but chairs are available,
+then the customer sits in one of the free chairs. If the barber is asleep, the
+customer wakes up the barber. Write a program to coordinate the barber and the customers.*/
+
+//Code By Rishabh Rai
+//Lovely Professional University
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
